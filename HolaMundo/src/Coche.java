119be12a8706d1e53cd372b098e0986bd64d7773
Adriana <a18adrianaabr@fpteis.es>
2019-10-01 19:13:59 +0200

    Con el mensaje cambiado

--- HolaMundo/src/Coche.java
+++ HolaMundo/src/Coche.java
@@ -2,7 +2,7 @@
 public class Coche {
 public static void main(String[] args) {
 		
-	System.out.println("Soy un coche");
+	System.out.println("Soy un coche, brum brum");
 
 	}
 
--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,10 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola Mundo!!!");
+		
+		
+		
+		new Coche();
 	}
 
 }
